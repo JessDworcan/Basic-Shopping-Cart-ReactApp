197e2634cf4fcd72a83d8074ad11f2573510260e
Jess Dworcan <jessdworcan@gmail.com>
2022-12-06 01:07:05 +0200

    Handling event onClick of btn, remove create to tag code

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -6,6 +6,26 @@ class Counter extends Component {
     tags: [],
   };
 
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClass()}> {this.formatCount()} </span>
+        <button
+          onClick={this.handleIncrement}
+          className={this.getButtonClass()}
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create new tag!  "}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  handleIncrement() {
+    console.log("Increment Clicked");
+  }
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
@@ -17,19 +37,15 @@ class Counter extends Component {
     );
   }
 
-  render() {
-    return (
-      <div>
-        {this.state.tags.length === 0 && "Please create new tag!"}
-        {this.renderTags()}
-      </div>
-    );
-  }
-
   getBadgeClass() {
     let badge1 = "badge m-2 badge-";
     badge1 += this.state.count === 0 ? "warning" : "primary";
     return badge1;
+  }
+
+  getButtonClass() {
+    let btn1 = "btn btn-secondary b1";
+    return btn1;
   }
 
   formatCount() {
