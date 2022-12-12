300b6723b6ffea2f7c4f381e6ff7a799e4f258b8
Jess Dworcan <jessdworcan@gmail.com>
2022-12-12 16:03:17 +0200

    lifting state up to App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,52 @@ import Counters from './components/counters';
 
 
 class App extends Component {
+
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    //console.log("Event Handler called", counterId);
+
+    // filter for ids not equal to the specified id
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    // overrite counters property at state level
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
             <Navbar />
       <main className='container'>
-        <Counters/>
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onIncrement={this.handleIncrement}
+          onDelete={this.handleDelete}
+          />
       </main>
       </React.Fragment>
       
